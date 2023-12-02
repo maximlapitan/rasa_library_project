0a345ea8660252cba7c96b3dfa43d50c35f4de9c
Maxim Zotov <maxim.zotov@stud.th-deg.de>
2023-12-02 12:19:50 +0100

    add more information and doctor info

--- webserver/server.py
+++ webserver/server.py
@@ -38,42 +38,4 @@ def get_student_by_id(student_id):
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=3000, debug=True)
-
-    # student:
-    #     student_mn:
-    #     name:
-    #     surname:
-    #     gpa:
-            
-    # courses:
-    #     course_id:
-    #     name:
-    #     ects: int
-    #     timetable: json text
-    #     program: 
-        
-    # student-course:
-    #     student_course_id:
-    #     course_id: course_id
-    #     student_mn: student_mn
-        
-    # student_course_status:
-    #     student_course_status_id:
-    #     student_course_id: student_course_id
-    #     status: [ATTENDING, COMPLETED, RETAKE, MUST_BE_TAKEN]
-        
-    # course_prerequisite:
-    #     course_prerequisite_id:
-    #     course_wish: course_id
-    #     course_to_take: course_id
-        
-    # faculty:
-    #     faculty_id:
-    #     name:
-            
-    # program:
-    #     program_id:
-    #     faculty_id:
-    #     program_name:
-            
         
